856b162d1327cad57c85cab18bea723bd2a515c0
Jordan Hart <p22jhart@fairfieldprep.org>
2022-01-11 07:17:19 -0800

    9-3

---
+++ lab_3-1.py
@@ -0,0 +1,18 @@
+#Jordan Hart
+
+try:
+
+    add_foods=int(input("input food"))
+    print(add_foods)
+
+except:
+    print("Invalid Input")
+finally:
+    print("have a good day")
+
+try:
+    sixth_letter=('o','e')
+    print("sixth_letter:",sixth_letter)
+    "sixth_letter"
+except:
+    print("invalid input")
